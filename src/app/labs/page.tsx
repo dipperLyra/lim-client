4ea182fa8488eba71a469a6130c6953a831d9c8b
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-09-11 19:48:46 +0100

    revert to showing location on lab card

--- src/app/labs/page.tsx
+++ src/app/labs/page.tsx
@@ -107,7 +107,12 @@ export default function LabSetup() {
               ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                   {labs.map((lab) => (
-                    <LaboratoryCard key={lab.id} id={lab.id} name={lab.name} />
+                    <LaboratoryCard
+                      key={lab.id}
+                      id={lab.id}
+                      name={lab.name}
+                      location={`${lab.state}`}
+                    />
                   ))}
                 </div>
               )}
