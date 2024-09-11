920522202e783fa7d4d715eaac9cc805cb85d549
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-09-11 11:24:19 +0100

    Remove location from lab card

--- src/app/components/cards/Laboratory.tsx
+++ src/app/components/cards/Laboratory.tsx
@@ -8,12 +8,13 @@ interface LaboratoryCardProps {
 
 const LaboratoryCard = ({ name, location, id }: LaboratoryCardProps) => {
     return (
-      <div className="bg-white rounded-lg p-4 shadow-md">
-        <h2 className="text-xl font-semibold mb-2">{name}</h2>
-        <p className="text-gray-700 mb-4">Location: {location}</p>
-        <Link href={`/labs/${id}`} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-          View Details
-        </Link>
+      <div className="bg-white rounded-lg p-4 shadow-md flex justify-center">
+        <div className="text-center">
+          <h2 className="text-xl font-semibold mb-2">{name}</h2>
+          <Link href={`/labs/${id}`} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            View Details
+          </Link>
+        </div>
       </div>
     );
   };
