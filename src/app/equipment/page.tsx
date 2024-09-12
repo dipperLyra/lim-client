8d80236ce64aaa3f90a458f85cc6f93623e31431
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-09-12 09:22:31 +0100

    Edit button

--- src/app/equipment/page.tsx
+++ src/app/equipment/page.tsx
@@ -89,7 +89,7 @@ export default function Equipment() {
                         href={`#`}
                         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
                       >
-                        Goto Lab
+                        Edit
                       </Link>
                     </td>
                   </tr>
