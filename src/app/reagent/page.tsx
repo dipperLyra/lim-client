3af75baff583e2645e8623efe59870466310a011
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-09-24 09:09:33 +0100

    troubleshooting github account issues

--- src/app/reagent/page.tsx
+++ src/app/reagent/page.tsx
@@ -118,7 +118,6 @@ export default function Reagent() {
       <div className="flex-1 flex flex-col overflow-y-auto">
         <DashboardHeader />
         <div className="container mx-auto p-4">
-          {/* Button to trigger modal */}
           <div className="flex justify-start mb-4">
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
               New Reagent
