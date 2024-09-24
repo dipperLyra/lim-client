e3f9f683312bd8e0a7ad4e21db089c128a9315f4
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-09-24 09:06:30 +0100

    Remove comments from header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,6 @@
 import Image from "next/image";
 import { User } from "lucide-react";
 
-// const DashboardHeader = () => {
-//   return (
-//     <header className="flex justify-between items-center p-5 bg-[#14203d] text-white">
-//       <a href="/dashboard">
-//         <Image src="/bloom-logo.png" alt="Bloom Logo" width={150} height={30} />
-//       </a>
-
-//       <div className="text-2xl">
-//         <span>&#128276;</span>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default DashboardHeader;
-
 const DashboardHeader = () => {
   return (
     <header className="flex justify-between items-center p-5 bg-gradient-to-r from-blue-900 to-indigo-900 text-white shadow-lg">
@@ -26,13 +10,8 @@ const DashboardHeader = () => {
 
       <div className="flex items-center space-x-6">
         <div className="flex flex-col items-center">
-          <div className="flex flex-row items-center">
-            <div className="bg-white rounded-full p-1 mb-1 mr-2">
-              <User className="text-blue-900" size={28} />
-            </div>
-            {/*<div className="text-2xl">
-              <span>&#128276;</span>
-            </div>*/}
+          <div className="bg-white rounded-full p-1 mb-1 mr-2">
+            <User className="text-blue-900" size={28} />
           </div>
           <div className="text-center">
             <div className="text-xl font-medium">Standard Organisation</div>
