78c8cb5aadf0ec3060d3293be0904b8a75ebd24b
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-09-29 21:30:22 +0100

    change header and sidepanel  bg color

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const DashboardHeader = () => {
   return (
-    <header className="flex justify-between items-center p-5 bg-[#14203d] text-white shadow-lg">
+    <header className="flex justify-between items-center p-5 bg-gradient-to-r from-blue-900 to-indigo-900 text-white shadow-lg">
       <a href="/dashboard" className="flex items-center space-x-2">
         <Image src="/bloom-logo.png" alt="Bloom Logo" width={150} height={30} />
       </a>
